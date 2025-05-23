2af303f492ce8f6f8f77bfa2f46c1c9a904769a3
Deydeepya <ddpyawork@gmail.com>
2025-05-23 15:07:07 +0300

    front/home page start

--- frontend_portfolio/src/app/page.js
+++ frontend_portfolio/src/app/page.js
@@ -1,16 +1,33 @@
 //home page of the application (as in what is displayed in Localhost:3000/)
-
 import { Typewriter } from './typewriter_text.tsx';
+import "../styles/fonts.css";
+// font imports
+
 
 export default function Home() {
   return (
-    <div>
-      <Typewriter
-        text={["Welcome to Deydeepya's Portfolio!" , "How may I assist you?"]}
-        speed={100}
-        loop={true}
-        className="text-xl font-medium"
-      />
+    <div className=''>
+      <div>
+        <h1 className="text-2xl font-light text-white"> Welcome </h1>
+        <h1 className="text-4xl font-medium text-white funnel-display-font"> I'm Deydeepya.</h1>
+        <h1 className="text-2xl font-normal italic antialiased  text-gray-400">(<Typewriter
+          text={["CS Student", "Developer", "Artist","Gamer"]}
+          speed={150}
+          loop={true}
+          className="text-2xl font-semibold italic antialiased text-gray-400" 
+        />)</h1>
+        <h2 className="text-4xl font-medium text-white">How may I assist you today?</h2>
+      </div>
+      <div>
+
+      </div>
+      <div>
+        <button></button>
+        <button></button>
+      </div>
+        
+
+
     </div> 
   );
 }
